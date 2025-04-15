5e101c19001ee81f57e7dc102582db422e8ed52a
piscinasatlantis <criguer4@gmail.com>
2025-04-15 17:09:42 -0400

    Add files via upload

---
+++ imsearch.php
@@ -0,0 +1,185 @@
+<?php require_once("res/x5engine.php"); ?>
+<!DOCTYPE html><!-- HTML5 -->
+<html prefix="og: http://ogp.me/ns#" lang="es-ES" dir="ltr">
+	<head>
+		<title>Buscar - Nuevo proyecto 2</title>
+		<meta charset="utf-8" />
+		<!--[if IE]><meta http-equiv="ImageToolbar" content="False" /><![endif]-->
+		<meta name="generator" content="Incomedia WebSite X5 Evo 2022.3.7 - www.websitex5.com" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		
+		<link rel="stylesheet" href="style/reset.css?2022-3-7-0" media="screen,print" />
+		<link rel="stylesheet" href="style/print.css?2022-3-7-0" media="print" />
+		<link rel="stylesheet" href="style/style.css?2022-3-7-0" media="screen,print" />
+		<link rel="stylesheet" href="style/template.css?2022-3-7-0" media="screen" />
+		
+		<link rel="stylesheet" href="pluginAppObj/imFooter_pluginAppObj_06/custom.css" media="screen, print" />
+		<link rel="stylesheet" href="pcss/imsearch.css?2022-3-7-0-638803268752288787" media="screen,print" />
+		<script src="res/jquery.js?2022-3-7-0"></script>
+		<script src="res/x5engine.js?2022-3-7-0" data-files-version="2022-3-7-0"></script>
+		
+		<script src="pluginAppObj/imFooter_pluginAppObj_06/main.js"></script>
+		<script>
+			window.onload = function(){ checkBrowserCompatibility('El Explorador que estás usando no es compatible con las funciones requeridas para mostrar este Sitio web.','El Navegador que estás utilizando podría no ser compatible con las funciones requeridas para poder ver este Sitio web.','[1]Actualiza tu explorador [/1] o [2]continuar de todos modos[/2].','http://outdatedbrowser.com/'); };
+			x5engine.utils.currentPagePath = 'imsearch.php';
+		</script>
+		
+	</head>
+	<body>
+		<div id="imPageExtContainer">
+			<div id="imPageIntContainer">
+				<div id="imHeaderBg"></div>
+				<div id="imFooterBg"></div>
+				<div id="imPage">
+					<header id="imHeader">
+						<h1 class="imHidden">Buscar - Nuevo proyecto 2</h1>
+						<div id="imHeaderObjects"><div id="imHeader_imMenuObject_02_wrapper" class="template-object-wrapper"><!-- UNSEARCHABLE --><div id="imHeader_imMenuObject_02"><div id="imHeader_imMenuObject_02_container"><div class="hamburger-button hamburger-component"><div><div><div class="hamburger-bar"></div><div class="hamburger-bar"></div><div class="hamburger-bar"></div></div></div></div><div class="hamburger-menu-background-container hamburger-component">
+	<div class="hamburger-menu-background menu-mobile menu-mobile-animated hidden">
+		<div class="hamburger-menu-close-button"><span>&times;</span></div>
+	</div>
+</div>
+<ul class="menu-mobile-animated hidden">
+	<li class="imMnMnFirst imPage" data-link-paths=",/index.html,/">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="index.html">
+Inicio		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths=",/construccion.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="construccion.html">
+Construccion		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths=",/mantencion-y-servicio-tecnico.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="mantencion-y-servicio-tecnico.html">
+Mantención y servicio técnico		</a>
+</div>
+</div>
+	</li><li class="imMnMnLast imPage" data-link-paths=",/contacto.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="contacto.html">
+Contacto		</a>
+</div>
+</div>
+	</li></ul></div></div><!-- UNSEARCHABLE END --><script>
+var imHeader_imMenuObject_02_settings = {
+	'menuId': 'imHeader_imMenuObject_02',
+	'responsiveMenuEffect': 'slide',
+	'responsiveMenuLevelOpenEvent': 'mouseover',
+	'animationDuration': 1000,
+}
+x5engine.boot.push(function(){x5engine.initMenu(imHeader_imMenuObject_02_settings)});
+$(function () {$('#imHeader_imMenuObject_02_container ul li').not('.imMnMnSeparator').each(function () {    var $this = $(this), timeout = 0;    $this.on('mouseenter', function () {        if($(this).parents('#imHeader_imMenuObject_02_container-menu-opened').length > 0) return;         clearTimeout(timeout);        setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeIn(); }, 250);    }).on('mouseleave', function () {        if($(this).parents('#imHeader_imMenuObject_02_container-menu-opened').length > 0) return;         timeout = setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeOut(); }, 250);    });});});
+
+</script>
+</div><div id="imHeader_imObjectImage_03_wrapper" class="template-object-wrapper"><div id="imHeader_imObjectImage_03"><div id="imHeader_imObjectImage_03_container"><img src="images/jefe-copia-2.png" title="" alt="" width="90" height="109" />
+</div></div></div><div id="imHeader_imObjectImage_04_wrapper" class="template-object-wrapper"><div id="imHeader_imObjectImage_04"><div id="imHeader_imObjectImage_04_container"><img src="images/logo-2.png" title="" alt="" width="80" height="88" />
+</div></div></div></div>
+					</header>
+					<div id="imStickyBarContainer">
+						<div id="imStickyBarGraphics"></div>
+						<div id="imStickyBar">
+							<div id="imStickyBarObjects"><div id="imStickyBar_imObjectTitle_02_wrapper" class="template-object-wrapper"><div id="imStickyBar_imObjectTitle_02"><span id ="imStickyBar_imObjectTitle_02_text" >Title</span></div></div><div id="imStickyBar_imMenuObject_03_wrapper" class="template-object-wrapper"><!-- UNSEARCHABLE --><div id="imStickyBar_imMenuObject_03"><div id="imStickyBar_imMenuObject_03_container"><div class="hamburger-button hamburger-component"><div><div><div class="hamburger-bar"></div><div class="hamburger-bar"></div><div class="hamburger-bar"></div></div></div></div><div class="hamburger-menu-background-container hamburger-component">
+	<div class="hamburger-menu-background menu-mobile menu-mobile-animated hidden">
+		<div class="hamburger-menu-close-button"><span>&times;</span></div>
+	</div>
+</div>
+<ul class="menu-mobile-animated hidden">
+	<li class="imMnMnFirst imPage" data-link-paths=",/index.html,/">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="index.html">
+Inicio		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths=",/construccion.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="construccion.html">
+Construccion		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths=",/mantencion-y-servicio-tecnico.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="mantencion-y-servicio-tecnico.html">
+Mantención y servicio técnico		</a>
+</div>
+</div>
+	</li><li class="imMnMnLast imPage" data-link-paths=",/contacto.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="contacto.html">
+Contacto		</a>
+</div>
+</div>
+	</li></ul></div></div><!-- UNSEARCHABLE END --><script>
+var imStickyBar_imMenuObject_03_settings = {
+	'menuId': 'imStickyBar_imMenuObject_03',
+	'responsiveMenuEffect': 'slide',
+	'responsiveMenuLevelOpenEvent': 'mouseover',
+	'animationDuration': 1000,
+}
+x5engine.boot.push(function(){x5engine.initMenu(imStickyBar_imMenuObject_03_settings)});
+$(function () {$('#imStickyBar_imMenuObject_03_container ul li').not('.imMnMnSeparator').each(function () {    var $this = $(this), timeout = 0, subtimeout = 0, width = 'none', height = 'none';        var submenu = $this.children('ul').add($this.find('.multiple-column > ul'));    $this.on('mouseenter', function () {        if($(this).parents('#imStickyBar_imMenuObject_03_container-menu-opened').length > 0) return;         clearTimeout(timeout);        clearTimeout(subtimeout);        $this.children('.multiple-column').show(0);        submenu.stop(false, false);        if (width == 'none') {             width = submenu.width();        }        if (height == 'none') {            height = submenu.height();            submenu.css({ overflow : 'hidden', height: 0});        }        setTimeout(function () {         submenu.css({ overflow : 'hidden'}).fadeIn(1).animate({ height: height }, 300, null, function() {$(this).css('overflow', 'visible'); });        }, 250);    }).on('mouseleave', function () {        if($(this).parents('#imStickyBar_imMenuObject_03_container-menu-opened').length > 0) return;         timeout = setTimeout(function () {         submenu.stop(false, false);            submenu.css('overflow', 'hidden').animate({ height: 0 }, 300, null, function() {$(this).fadeOut(0); });            subtimeout = setTimeout(function () { $this.children('.multiple-column').hide(0); }, 300);        }, 250);    });});});
+
+</script>
+</div></div>
+						</div>
+					</div>
+					<a class="imHidden" href="#imGoToCont" title="Salta el menu principal">Vaya al Contenido</a>
+					<div id="imSideBar">
+						<div id="imSideBarObjects"><div id="imSideBar_imObjectImage_01_wrapper" class="template-object-wrapper"><div id="imSideBar_imObjectImage_01"><div id="imSideBar_imObjectImage_01_container"><img src="images/empty-GT_imagea-1-.png" title="" alt="" width="140" height="140" />
+</div></div></div></div>
+					</div>
+					<div id="imContentGraphics"></div>
+					<main id="imContent">
+						<a id="imGoToCont"></a>
+						<div id="imSearchPage">
+						<h2 id="imPgTitle">Resultados de búsqueda</h2>
+						<?php
+						$search = new imSearch();
+						$keys = isset($_GET['search']) ? @htmlspecialchars($_GET['search'], ENT_COMPAT) : "";
+						$page = isset($_GET['page']) ? @htmlspecialchars($_GET['page']) : 0;
+						$type = isset($_GET['type']) ? @htmlspecialchars($_GET['type']) : "pages"; ?>
+						<div class="searchPageContainer">
+						<?php echo $search->search($keys, $page, $type); ?>
+						</div>
+						</div>
+						
+					</main>
+					<footer id="imFooter">
+						<div id="imFooterObjects"><div id="imFooter_imObjectTitle_02_wrapper" class="template-object-wrapper"><div id="imFooter_imObjectTitle_02"><span id ="imFooter_imObjectTitle_02_text" >Piscinas Atlantis</span></div></div><div id="imFooter_imTextObject_03_wrapper" class="template-object-wrapper"><div id="imFooter_imTextObject_03">
+	<div data-index="0"  class="text-tab-content grid-prop current-tab "  id="imFooter_imTextObject_03_tab0" style="opacity: 1; ">
+		<div class="text-inner">
+			<div data-line-height="1.15" class="lh1-15"><span class="fs10lh1-15"><b><span class="cf1">Punta Arenas 10419</span></b></span></div><div data-line-height="1.15" class="lh1-15"><span class="fs10lh1-15"><b><span class="cf1">La Florida, Santiago, Chile</span></b></span></div><div data-line-height="1.15" class="lh1-15"><span class="fs10lh1-15"><b><span class="cf1">+56 9 91328119</span></b></span></div><div data-line-height="1.15" class="lh1-15"><span class="fs10lh1-15"><b><span class="cf1">piscinasatlantis@vtr.net</span></b></span><br></div>
+		</div>
+	</div>
+
+</div>
+</div><div id="imFooter_pluginAppObj_06_wrapper" class="template-object-wrapper"><!-- Social Icons v.17 --><div id="imFooter_pluginAppObj_06">
+            <div id="soc_imFooter_pluginAppObj_06">
+                <div class="wrapper horizontal original none">
+                	<div class='social-icon original'><a href='https://fb.me/Incomedia' target='_blank'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><g><rect width="100" height="100" style="fill:#3d5a98"/><g><path d="M69,100V61.27H82l1.95-15.09H69V36.55c0-4.37,1.21-7.35,7.48-7.35h8V15.7a106.94,106.94,0,0,0-11.65-.59c-11.52,0-19.41,7-19.41,19.95V46.18h-13V61.27h13V100H69Z" style="fill:#fff"/></g></g></svg><span class='fallbacktext'>Fb</span></a></div><div class='social-icon original'><a href='https://www.youtube.com/user/incomedia' target='_blank'><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 100 100"><defs><radialGradient id="shade_ig_imFooter_pluginAppObj_06" cx="18.44" cy="364.72" r="115" gradientTransform="matrix(1, 0, 0, -1, 0, 460.03)" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#ffb140"/><stop offset="0.26" stop-color="#ff5445"/><stop offset="0.6" stop-color="#fc2b82"/><stop offset="1" stop-color="#8e40b7"/></radialGradient></defs><g><g><rect x="5" y="5" width="90" height="90" style="fill:none"/><path d="M79.42,26a5.4,5.4,0,1,1-5.4-5.4A5.4,5.4,0,0,1,79.42,26ZM50,65A15,15,0,1,1,65,50,15,15,0,0,1,50,65Zm0-38.11A23.11,23.11,0,1,0,73.11,50,23.11,23.11,0,0,0,50,26.89Zm0-13.78c12,0,13.44,0,18.18.26a24.89,24.89,0,0,1,8.36,1.55,13.94,13.94,0,0,1,5.17,3.37,13.93,13.93,0,0,1,3.37,5.17,24.91,24.91,0,0,1,1.55,8.36c0.22,4.75.26,6.17,0.26,18.18s0,13.44-.26,18.18a24.89,24.89,0,0,1-1.55,8.36,14.91,14.91,0,0,1-8.54,8.54,24.91,24.91,0,0,1-8.36,1.55C63.44,86.85,62,86.89,50,86.89s-13.44,0-18.18-.26a24.89,24.89,0,0,1-8.36-1.55,13.94,13.94,0,0,1-5.17-3.37,13.94,13.94,0,0,1-3.37-5.17,24.91,24.91,0,0,1-1.55-8.36C13.15,63.44,13.11,62,13.11,50s0-13.44.26-18.18a24.89,24.89,0,0,1,1.55-8.36,13.94,13.94,0,0,1,3.37-5.17,13.93,13.93,0,0,1,5.17-3.37,24.91,24.91,0,0,1,8.36-1.55C36.56,13.15,38,13.11,50,13.11ZM50,5c-12.22,0-13.75.05-18.55,0.27A33,33,0,0,0,20.52,7.36a22.05,22.05,0,0,0-8,5.19,22.05,22.05,0,0,0-5.19,8A33,33,0,0,0,5.27,31.45C5.05,36.25,5,37.78,5,50s0.05,13.75.27,18.55A33,33,0,0,0,7.36,79.48a22.05,22.05,0,0,0,5.19,8,22.06,22.06,0,0,0,8,5.19,33,33,0,0,0,10.92,2.09C36.25,94.95,37.78,95,50,95s13.75-.05,18.55-0.27a33,33,0,0,0,10.92-2.09A23,23,0,0,0,92.64,79.48a33,33,0,0,0,2.09-10.92C94.95,63.75,95,62.22,95,50s-0.05-13.75-.27-18.55a33,33,0,0,0-2.09-10.92,22.06,22.06,0,0,0-5.19-8,22.06,22.06,0,0,0-8-5.19A33,33,0,0,0,68.55,5.27C63.75,5.05,62.22,5,50,5Z" style="fill:url(#shade_ig_imFooter_pluginAppObj_06)"/></g></g></svg><span class='fallbacktext'>Ig</span></a></div><div class='social-icon original'><a href='https://twitter.com/Incomedia' target='_blank'><svg width="100%" height="100%" viewBox="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"><g id="tw_original"><rect x="0" y="0" width="100" height="100"/><g transform="matrix(0.233139,0,0,0.233139,15.0291,15)"><path d="M178.57,127.15L290.27,0L263.81,0L166.78,110.38L89.34,0L0,0L117.13,166.93L0,300.25L26.46,300.25L128.86,183.66L210.66,300.25L300,300.25M36.01,19.54L76.66,19.54L263.79,281.67L223.13,281.67" style="fill:white;fill-rule:nonzero;"/></g></g></svg><span class='fallbacktext'>X</span></a></div>
+                </div>
+
+            </div>
+                <script>
+                    socialicons_imFooter_pluginAppObj_06();
+                </script>
+        </div></div></div>
+					</footer>
+				</div>
+				<span class="imHidden"><a href="#imGoToCont" title="Lea esta página de nuevo">Regreso al contenido</a></span>
+			</div>
+		</div>
+		
+		<noscript class="imNoScript"><div class="alert alert-red">Para utilizar este sitio tienes que habilitar JavaScript.</div></noscript>
+	</body>
+</html>
