55726fc2a86070a285fedffb275518eda1a61428
piscinasatlantis <criguer4@gmail.com>
2025-04-16 11:21:19 -0400

    Add files via upload

---
+++ res/x5settings.php
@@ -0,0 +1,119 @@
+<?php
+
+/*
+|-------------------------------
+|	GENERAL SETTINGS
+|-------------------------------
+*/
+
+$imSettings['general'] = array(
+	'site_id' => 'B1C2EF24C4C4C9CBFEDB427FD033605F',
+	'url' => 'http://localhost/',
+	'homepage_url' => 'http://localhost/index.html',
+	'icon' => '',
+	'version' => '2022.3.7.0',
+	'sitename' => 'Nuevo proyecto 2',
+	'lang_code' => 'es-ES',
+	'public_folder' => '',
+	'salt' => 'k9bn852eycer8xi2cfrc06rwihaznj9vg',
+	'common_email_sender_addres' => 'crisguerycatita@gmail.com'
+);
+/*
+|-------------------------------
+|	PASSWORD POLICY
+|-------------------------------
+*/
+
+$imSettings['password_policy'] = array(
+	'required_policy' => false,
+	'minimum_characters' => '6',
+	'include_uppercase' => false,
+	'include_numeric' => false,
+	'include_special' => false
+);
+/*
+|-------------------------------
+|	Captcha
+|-------------------------------
+*/ImTopic::$captcha_code = "		<div class=\"x5captcha-wrap\">
+			<label for=\"1ftyodg9-imCpt\">Palabra de control:</label><br />
+			<input type=\"text\" id=\"1ftyodg9-imCpt\" class=\"imCpt\" name=\"imCpt\" maxlength=\"5\" />
+		</div>
+";
+
+
+$imSettings['admin'] = array(
+	'icon' => 'admin/images/logo_q8dufm0j.png',
+	'theme' => 'orange',
+	'extra-dashboard' => array(),
+	'extra-links' => array()
+);
+
+
+/*
+|--------------------------------------------------------------------------------------
+|	DATABASES SETTINGS
+|--------------------------------------------------------------------------------------
+*/
+
+$imSettings['databases'] = array();
+$ecommerce = Configuration::getCart();
+$ecommerce->setSettings(array(
+	'page_url' => 'http://localhost/',
+	'force_sender' => false,
+	'mail_btn_css' => 'display: inline-block; text-decoration: none; color: rgba(255, 255, 255, 1); background-color: rgba(17, 85, 204, 1); padding: 6px 15px 6px 15px; border-style: solid; border-width: 0px 0px 0px 0px; border-color: rgba(55, 71, 79, 1) rgba(55, 71, 79, 1) rgba(55, 71, 79, 1) rgba(55, 71, 79, 1); border-top-left-radius: 18px; border-top-right-radius: 18px; border-bottom-left-radius: 18px; border-bottom-right-radius: 18px;',
+	'email_opening' => 'Estimado cliente,<br /><br />Gracias por su pedido Se le recordamos de que el pago no ha todavía sido recibido.<br /><br />A continuación encontrará una lista de los productos pedidos, la información de facturación y de envío y las instrucciones para efectuar el pago.',
+	'email_closing' => 'Póngase en contacto con nosotros si requiere información adicional.<br /><br />Atentamente, el personal de Ventas.',
+	'useCSV' => false,
+	'header_bg_color' => 'rgba(37, 58, 88, 1)',
+	'header_text_color' => 'rgba(255, 255, 255, 1)',
+	'cell_bg_color' => 'rgba(255, 255, 255, 1)',
+	'cell_text_color' => 'rgba(0, 0, 0, 1)',
+	'border_color' => 'rgba(211, 211, 211, 1)',
+	'owner_email' => 'example@example.com',
+	'vat_type' => 'included',
+	'availability_image' => ''
+));
+
+$ecommerce->setPriceFormatData(array(
+	'decimals' => 2,
+	'decimal_sep' => '.',
+	'thousands_sep' => '',
+	'currency_to_right' => true,
+	'currency_separator' => ' ',
+	'show_zero_as' => '0',
+	'currency_symbol' => '€',
+	'currency_code' => 'EUR',
+	'currency_name' => 'Euro',
+));
+
+
+/*
+|-------------------------------------------------------------------------------------------
+|	GUESTBOOK SETTINGS
+|-------------------------------------------------------------------------------------------
+*/
+
+$imSettings['guestbooks'] = array();
+
+
+/*
+|-------------------------------
+|	EMAIL SETTINGS
+|-------------------------------
+*/
+
+$ImMailer->emailType = 'phpmailer';
+$ImMailer->header = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">' . "\n" . '<html>' . "\n" . '<head>' . "\n" . '<meta http-equiv="content-type" content="text/html; charset=utf-8">' . "\n" . '<meta name="generator" content="Incomedia WebSite X5 Evolution 2022.3.7 - www.websitex5.com">' . "\n" . '</head>' . "\n" . '<body bgcolor="#37474F" style="background-color: #37474F;">' . "\n\t" . '<table border="0" cellpadding="0" align="center" cellspacing="0" style="padding: 0; margin: 0 auto; width: 700px;">' . "\n\t" . '<tr><td id="imEmailContent" style="min-height: 300px; padding: 10px; font: normal normal normal 9pt \'Open Sans\'; color: #000000; background-color: #FFFFFF; text-decoration: none; text-align: left; width: 700px; border-style: solid; border-color: #000000; border-top-width: 1px; border-right-width: 1px; border-bottom-width: 1px; border-left-width: 1px; background-color: #FFFFFF" width="700px">' . "\n\t\t";
+$ImMailer->footer = "\n\t" . '</td></tr>' . "\n\t" . '</table>' . "\n" . '<table width="100%"><tr><td id="imEmailFooter" style="font: normal normal normal 7pt \'Open Sans\'; color: #FFFFFF; background-color: transparent; text-decoration: none; text-align: center;  padding: 10px; margin-top: 5px;background-color: transparent">' . "\n\t\t" . 'Este e-mail incluye información exclusiva para el destinatario mencionado anteriormente.<br>Si lo ha recibido por error, notifíqueselo inmediatamente al remitente y destrúyalo sin copiarlo.' . "\n\t" . '</td></tr></table>' . "\n\t" . '</body>' . "\n" . '</html>';
+$ImMailer->bodyBackground = '#FFFFFF';
+$ImMailer->bodyBackgroundEven = '#FFFFFF';
+$ImMailer->bodyBackgroundOdd = '#F0F0F0';
+$ImMailer->bodyBackgroundBorder = '#CDCDCD';
+$ImMailer->bodyTextColorOdd = '#000000';
+$ImMailer->bodySeparatorBorderColor = '#000000';
+$ImMailer->emailBackground = '#37474F';
+$ImMailer->emailContentStyle = 'font: normal normal normal 9pt \'Open Sans\'; color: #000000; background-color: #FFFFFF; text-decoration: none; text-align: left; ';
+$ImMailer->emailContentFontFamily = 'font-family: Open Sans;';
+
+// End of file x5settings.php
